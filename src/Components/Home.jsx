46ff1184397e10da806b21187b099d48237a3086
AnanthuSuresh098 <93374262+AnanthuSuresh098@users.noreply.github.com>
2023-02-08 14:16:18 +0530

    slider update

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,69 +1,9 @@
 import React, { Component } from "react";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import Slider from "react-slick";
 
 export const Home = () => {
 
-     var settings = {
-       dots: true,
-       infinite: true,
-       slidesToShow: 2,
-       slidesToScroll: 1,
-       autoplay: true,
-       speed: 2000,
-       responsive: [
-         {
-           breakpoint: 480,
-           settings: {
-             slidesToShow: 3,
-             slidesToScroll: 3,
-             dots: false,
-           },
-         },
-       ],
-     };
-
   return (
     <div id="homepage-main-wrapper">
-      {/* Homepage slider 01 starts here */}
-
-      <div id="home-banner-slider-wrap">
-        <Slider {...settings} id="home-banner-slider-element" width="80%">
-          <div className="home-banner-slider">
-            <img src="./slider.jpg" alt="banner slider" />
-          </div>
-          <div className="home-banner-slider">
-            <img src="./slider.jpg" alt="banner slider" />
-          </div>
-          <div className="home-banner-slider">
-            <img src="./slider.jpg" alt="banner slider" />
-          </div>
-          <div className="home-banner-slider">
-            <img src="./slider.jpg" alt="banner slider" />
-          </div>
-          <div className="home-banner-slider">
-            <img src="./slider.jpg" alt="banner slider" />
-          </div>
-          <div className="home-banner-slider">
-            <img src="./slider.jpg" alt="banner slider" />
-          </div>
-          <div className="home-banner-slider">
-            <img src="./slider.jpg" alt="banner slider" />
-          </div>
-          <div className="home-banner-slider">
-            <img src="./slider.jpg" alt="banner slider" />
-          </div>
-          <div className="home-banner-slider">
-            <img src="./slider.jpg" alt="banner slider" />
-          </div>
-          <div className="home-banner-slider">
-            <img src="./slider.jpg" alt="banner slider" />
-          </div>
-        </Slider>
-      </div>
-
-      {/* Homepage slider 01 ends here */}
     </div>
   );
 };
