942055bfeab57656616685b0c49a3793a0f8a526
AnanthuSuresh098 <93374262+AnanthuSuresh098@users.noreply.github.com>
2023-02-27 12:30:22 +0530

    partystore initial commit after design change

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,20 +5,7 @@ import { FiShoppingCart } from "react-icons/fi";
 export const Navbar = () => {
   return (
     <div id="navbar-main-wrapper">
-      <div id="navbar-logo-wrap">
-        <img src="./logo.png" alt="" id="navbar-logo-image" />
-      </div>
-      <div className="navbar-elements">Themes</div>
-      <div className="navbar-elements">Party Supplies</div>
-      <div className="navbar-elements">Tableware</div>
-      <div className="navbar-elements">Shop by Occasion</div>
-      <div className="navbar-elements">Accessories</div>
-      <div>
-        <FiUser className="Navbar-elements-user-icons"/>
-      </div>
-      <div>
-        <FiShoppingCart className="Navbar-elements-user-icons" />
-      </div>
+      
     </div>
   );
 };
