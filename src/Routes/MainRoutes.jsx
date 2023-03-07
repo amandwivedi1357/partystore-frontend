6741efd2e152eceb8485905ae57a52077060763b
AnanthuSuresh098 <93374262+AnanthuSuresh098@users.noreply.github.com>
2023-03-07 19:21:02 +0530

    celebrations page commit

--- src/Routes/MainRoutes.jsx
+++ src/Routes/MainRoutes.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import { Home } from "../Components/Home";
+import { Celebrations } from "../Components/Celebrations";
 
 export const MainRoutes = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/celebrations" element={<Celebrations />} />
       </Routes>
     </>
   );
